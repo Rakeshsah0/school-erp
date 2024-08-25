1355fe17f5751a0b899b59885ecf5404afb312a5
Rakeshsah0 <78860663+Rakeshsah0@users.noreply.github.com>
2024-08-25 20:15:46 +0530

    count chart prepared using recharts

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,25 +1,35 @@
-import UserCard from "@/components/UserCard"
-
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
   return (
     <div className="p-4 flex flex-col gap-4 md:flex-row">
       {/* LEFT */}
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* User cards */}
         <div className="flex gap-4 justify-between flex-wrap">
-          <UserCard type="student"/>
-          <UserCard type="teacher"/>
-          <UserCard type="parent"/>
-          <UserCard type="staff"/>
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
         </div>
+        {/* middle charts */}
+        <div className="flex flex-col lg:flex-row gap-4">
+          {/* count chart */}
+          <div className="w-full lg:w-1/3 h-[400px]">
+            <CountChart/>
+          </div>
+          {/* attendance chart  */}
+          <div className="w-full lg:w-2/3 h-[400px]"></div>
+        </div>
+        {/* lower chart */}
+        <div></div>
       </div>
 
       {/* RIGHT */}
-      <div className=" w-full lg:w-1/3">
-        Right
-      </div>
+      <div className=" w-full lg:w-1/3">Right</div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
---
+++ src/components/CountChart.tsx
@@ -0,0 +1,73 @@
+"use client";
+import Image from "next/image";
+import {
+  RadialBarChart,
+  RadialBar,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+    {
+        name: "Total",
+        count: 200,
+        fill: "white",
+      },
+  {
+    name: "Girls",
+    count: 66,
+    fill: "#FAE27E",
+  },
+  {
+    name: "Boys",
+    count: 134,
+    fill: "#C3EBFA",
+  },
+];
+
+const CountChart = () => {
+  return (
+    <div className="bg-white w-full h-full p-4 rounded-xl">
+      {/* Title  */}
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Students</h1>
+        <Image src="/moreDark.png" alt="" height={20} width={20} />
+      </div>
+      {/* chart  */}
+      <div className="relative w-full h-[75%]">
+        <ResponsiveContainer>
+          <RadialBarChart
+            cx="50%"
+            cy="50%"
+            innerRadius="40%"
+            outerRadius="100%"
+            barSize={32}
+            data={data}
+          >
+            <RadialBar
+              label={{ position: "insideStart", fill: "#fff" }}
+              background
+              dataKey="count"
+            />
+          </RadialBarChart>
+        </ResponsiveContainer>
+        <Image src="/maleFemale.png" alt="" height={50} width={50} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
+      </div>
+      {/* buttom  */}
+      <div className="flex justify-center gap-16">
+        <div className="flex flex-col gap-1">
+          <div className="rounded-full bg-rkSky w-5 h-5" />
+          <h1 className="font-bold">1,234</h1>
+          <h2 className="text-xs text-gray-500">Boys(67%)</h2>
+        </div>
+        <div className="flex flex-col gap-1">
+          <div className="rounded-full bg-rkYellow w-5 h-5" />
+          <h1 className="font-bold">678</h1>
+          <h2 className="text-xs text-gray-500">Girls(33%)</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountChart;

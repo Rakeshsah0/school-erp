4b6a0ec8ce41cc38d68695045b6e4f4e30b2bf79
Rakeshsah0 <78860663+Rakeshsah0@users.noreply.github.com>
2024-08-27 18:42:15 +0530

    navbar remade

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,5 @@
 import Menu from "@/components/Menu";
 import Navbar from "@/components/Navbar";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function DashboardLayout({
   children,
@@ -9,20 +7,18 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-screen flex">
-      {/* LEFT */}
-      <div className="w-1/6 md:w-[8%] lg:w-[16%] xl:w-[15%] p-4 flex flex-col h-full">
-        <Link href='/' className="flex items-center justify-center lg:justify-start gap-2">
-          <Image src="/logo.png" alt="" width={32} height={32}/>
-          <span className="hidden lg:block font-bold">RK School</span>
-        </Link>
-        <Menu/>
+    <>
+      <Navbar />
+      <div className="h-screen flex">
+        {/* LEFT */}
+        <div className="w-1/6 md:w-[8%] lg:w-[16%] xl:w-[15%] p-4 flex flex-col h-full">
+          <Menu />
+        </div>
+        {/* RIGHT */}
+        <div className="w-5/6 md:w-[92%] lg:w-[84%] xl:w-[85%] bg-[#F7F8FA] overflow-y-auto">
+          {children}
+        </div>
       </div>
-      {/* RIGHT */}
-      <div className="w-5/6 md:w-[92%] lg:w-[84%] xl:w-[85%] bg-[#F7F8FA] overflow-y-auto">
-        <Navbar/>
-        {children}
-      </div>
-    </div>
+    </>
   );
 }
